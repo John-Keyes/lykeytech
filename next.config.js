39b3db92a3c09833bbaf7aaaa8b98da638bb3b9c
John <johnschool432@gmail.com>
2025-04-07 11:27:02 -0400

    Build fix, and yml edit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 let clientUrl = process.env.CLIENT_URL;
-let apiUrl = process.env.API_URL;
 if(process.env.MODE == "dev") {
   clientUrl = "http://localhost";
-  apiUrl = "http://localhost";
   clientUrl = `${clientUrl}:${process.env.CLIENT_PORT}`;
-  apiUrl = `${apiUrl}:${process.env.API_PORT}`;
 }
 module.exports = {
 	reactStrictMode: true,
