95c31cf89f4290f7ca7d7798ea022941a4e3a92e
John <johnschool432@gmail.com>
2025-04-07 16:08:44 -0400

    fixing URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const mode = process.env.NODE_ENV;
 module.exports = {
-  basePath: mode == "production" ? "/nextjs-blog-deployment" : "", 
+  basePath: mode == "production" ? "/lykeytech" : "", 
 	reactStrictMode: true,
 	output: "standalone"
 };
