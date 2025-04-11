e878bb9fc185863f79eea8714dd71de0c81dc5a4
John <johnschool432@gmail.com>
2025-04-11 16:52:32 -0400

    Testing url change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 
-const clientUrl = process.env.NODE_ENV == "production" ? "/lykeytech" : "";
+//const clientUrl = process.env.NODE_ENV == "production" ? "/lykeytech" : "";
+const clientUrl = "";
 module.exports = {
   basePath: clientUrl,
 	reactStrictMode: true,
