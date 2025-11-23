673a772ad91024fb9a46bb471d8f27b9a2ec9d25
John <johnschool432@gmail.com>
2025-11-22 20:31:46 -0500

    home

--- svkit/src/routes/+page.svelte
+++ svkit/src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script lang="ts">
+
+    /*import SubTitle from 'src/components/layout/subTitle';
+    import Endorsement from 'src/components/layout/endorsement';
+    import Socials from 'src/components/layout/socials';
+    import PopMenu from 'src/components/popMenu';
+    import Tag from 'src/components/tag';
+    import About from 'src/components/layout/about';
+    import { codeCS, cmscritic, todoapp, resilience } from 'src/lib/helpers/static';
+    import Sample from 'src/components/layout/sample';
+    import Loading from 'src/components/layout/loading';*/
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+    
+<style>
+</style>
